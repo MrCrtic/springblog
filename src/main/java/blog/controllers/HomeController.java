19908b4b0202d94e47fb784a8dc56ffbe40ac21a
MrCrtic <36117404+MrCrtic@users.noreply.github.com>
2018-03-26 11:15:18 +0200

    Update HomeController.java

--- src/main/java/blog/controllers/HomeController.java
+++ src/main/java/blog/controllers/HomeController.java
@@ -26,77 +26,77 @@ import blog.services.UserService;
 
 @Controller
 public class HomeController {
-	@Autowired
-	private PostService postService;
-	@Autowired 
-	private UserService userService;
-	@Autowired 
-	private UserRoleService userRole;
-	@GetMapping("/")
-	public String index(Model model) {
-		List <Post> latest5 = postService.findLatest5();
-		model.addAttribute("latest5",latest5);
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-	      String username = auth.getName(); //get logged in username
-	      if(username != null) {
-	    	  model.addAttribute("username",username);
-	      }else {
-	    	  model.addAttribute("username",null);
-	      }
-	     
-		return "index";
-	}
-	
-	@GetMapping("/users")
-	public String allUsers(Model model) {
-		List <User> users = this.userService.findAll();
-		model.addAttribute("users",users);
-		
-		return "allusers";
-	}
-	@PostMapping("/add-user")
-	public String addUser(@ModelAttribute User user,@ModelAttribute UserRole ur, @RequestParam(name="username")String username,@RequestParam(name="fullname")String fullname,@RequestParam(name="password")String password) {
-		//Create a new password encoder
-		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    @Autowired
+    private PostService postService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRoleService userRole;
+    @GetMapping("/")
+    public String index(Model model) {
+        List < Post > latest5 = postService.findLatest5();
+        model.addAttribute("latest5", latest5);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName(); //get logged in username
+        if (username != null) {
+            model.addAttribute("username", username);
+        } else {
+            model.addAttribute("username", null);
+        }
 
-		//Encode the password
-//		String password = "test123";
-		String hashedPassword = passwordEncoder.encode(password);
+        return "index";
+    }
 
-		//Create the user with the encoded password
-	 user = new User( username,fullname, hashedPassword);
-	 userService.create(user);
-	  user = this.userService.findByUsername(username);
-	 ur = new UserRole(user.getId(),"ROLE_USER");
-	userRole.create(ur);
+    @GetMapping("/users")
+    public String allUsers(Model model) {
+        List < User > users = this.userService.findAll();
+        model.addAttribute("users", users);
 
-		//then persist
-	
-		return "/login";
-	}
-	@GetMapping("/register")
-	public String Register() {
-		return "/register";
-	}
-	@GetMapping("/delete_user")
-	public String deleteUser(@RequestParam(name="id")Long id) {
-	   this.userService.deleteById(id);
-	   return "redirect:/users";
-	}
-	@GetMapping("/add-admin")
-	public String addAdmin(Model model, @ModelAttribute UserRole ur, @RequestParam(name="id")Long id) {
-	Long id2 = this.userRole.findByuserid(id);
-	ur = this.userRole.find(id2);
-	if(id2!=null) {
-	this.userRole.delete(id2);
-	}
-	UserRole ur2 = new UserRole(id,"ROLE_ADMIN");
-	this.userRole.create(ur2);
-	
-	
-	
-	return "redirect:/users";
-	}
-	
-	
+        return "allusers";
+    }
+    @PostMapping("/add-user")
+    public String addUser(@ModelAttribute User user, @ModelAttribute UserRole ur, @RequestParam(name = "username") String username, @RequestParam(name = "fullname") String fullname, @RequestParam(name = "password") String password) {
+        //Create a new password encoder
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        //Encode the password
+        //		String password = "test123";
+        String hashedPassword = passwordEncoder.encode(password);
+
+        //Create the user with the encoded password
+        user = new User(username, fullname, hashedPassword);
+        userService.create(user);
+        user = this.userService.findByUsername(username);
+        ur = new UserRole(user.getId(), "ROLE_USER");
+        userRole.create(ur);
+
+        //then persist
+
+        return "/login";
+    }
+    @GetMapping("/register")
+    public String Register() {
+        return "/register";
+    }
+    @GetMapping("/delete_user")
+    public String deleteUser(@RequestParam(name = "id") Long id) {
+        this.userService.deleteById(id);
+        return "redirect:/users";
+    }
+    @GetMapping("/add-admin")
+    public String addAdmin(Model model, @ModelAttribute UserRole ur, @RequestParam(name = "id") Long id) {
+        Long id2 = this.userRole.findByuserid(id);
+        ur = this.userRole.find(id2);
+        if (id2 != null) {
+            this.userRole.delete(id2);
+        }
+        UserRole ur2 = new UserRole(id, "ROLE_ADMIN");
+        this.userRole.create(ur2);
+
+
+
+        return "redirect:/users";
+    }
+
+
 }
